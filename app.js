8cc900763efe1486944247b119f4adc0dff1850a
isMaxVelychko <ismaxvelychko@gmail.com>
2017-01-29 23:11:54 +0200

    custom ajax

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 
 getPokemons().then(function(response) {
     var list = document.getElementById('list');
-    response.results.forEach(function(item) {
-        var li = document.createElement('li');
-        var text = document.createTextNode(item.name);
-        li.appendChild(text);
-        list.appendChild(li);
-    })
+
+    var data = JSON.parse(response);
+    data.results.forEach(function(item) {
+         var li = document.createElement('li');
+         var text = document.createTextNode(item.name);
+         li.appendChild(text);
+         list.appendChild(li);
+     })
 });
--- services.js
+++ services.js
@@ -1,4 +1,29 @@
 
+var apiUrl = 'http://pokeapi.co/api/v2/';
+
 function getPokemons() {
-    return $.get( "http://pokeapi.co/api/v2/pokemon", {limit: 12});
+    var queryUrl = apiUrl + 'pokemon?limit=12';
+    return makeQuery('GET', queryUrl);
 }
+
+function makeQuery(method, url) {
+    return new Promise(function(resolve, reject) {
+        var req = new XMLHttpRequest();
+        req.open(method, url);
+        req.onload = onLoad;
+        req.onerror = onError;
+        req.send();
+
+        function onLoad() {
+            if(req.status == 200) {
+                resolve(this.response);
+            } else {
+                reject(this.responseText);
+            }
+        }
+
+        function onError() {
+            reject(Error('Network Error'));
+        }
+    });
+};
