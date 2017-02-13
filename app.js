6d7ef428a69a8ad46cb1a85eec1bdbd2c302ec45
isMaxVelychko <ismaxvelychko@gmail.com>
2017-02-13 21:59:27 +0200

    refacotring"

--- app.js
+++ app.js
@@ -4,98 +4,103 @@ import filter from './scripts/filter';
 import storage from './scripts/storage';
 import {setBackground} from './scripts/domManipulation';
 
-let nextListLink = null;
-let typeCollection = [];
+(function App() {
+    let nextListLink = null;
+    let typeCollection = [];
 
-drawListPokemons();
-initFavorites();
+    drawListPokemons();
+    initFavorites();
 
-document.addEventListener("DOMContentLoaded", function() {
-    let loadMoreElement = document.getElementById('loadMore');
-    let formElement = document.getElementById('form');
-    loadMoreElement.onclick = loadMore;
-    formElement.onsubmit = onSubmit;
-});
+    document.addEventListener("DOMContentLoaded", function() {
+        let loadMoreElement = document.getElementById('loadMore');
+        let formElement = document.getElementById('form');
+        loadMoreElement.onclick = loadMore;
+        formElement.onsubmit = onSubmit;
+    });
 
 
-function drawListPokemons(url) {
-    getPokemons(url).then(response => {
-        let list = document.getElementById('list');
-        let linkCollection = [];
+    function drawListPokemons(url) {
+        getPokemons(url).then(response => {
+            let list = document.getElementById('list');
+            let linkCollection = [];
 
-        let data = JSON.parse(response);
-        nextListLink = data.next;
-        data.results.forEach(item => {
-            let li = document.createElement('li');
-            let btn = document.createElement('button');
-            let text = document.createTextNode(item.name);
-            btn.innerHTML = 'add to favorite';
-            btn.setAttribute('name', item.name);
-            btn.onclick = addToFavorite;
-            li.appendChild(text);
-            li.appendChild(btn);
-            li.setAttribute('name', item.name);
-            list.appendChild(li);
-            linkCollection.push(item.url);
-        });
-        return linkCollection;
-    }).then(response => {
-        response.forEach(link => {
-            setTypeCollection(link);
+            let data = JSON.parse(response);
+            nextListLink = data.next;
+            data.results.forEach(item => {
+                let li = document.createElement('li');
+                let btn = document.createElement('button');
+                let text = document.createTextNode(item.name);
+                btn.innerHTML = 'add to favorite';
+                btn.setAttribute('name', item.name);
+                btn.onclick = addToFavorite;
+                li.appendChild(text);
+                li.appendChild(btn);
+                li.setAttribute('name', item.name);
+                list.appendChild(li);
+                linkCollection.push(item.url);
+            });
+            return linkCollection;
+        }).then(response => {
+            response.forEach(link => {
+                setTypeCollection(link);
+            })
         })
-    })
-}
+    }
 
-function loadMore() {
-    drawListPokemons(nextListLink);
-}
+    function loadMore() {
+        drawListPokemons(nextListLink);
+    }
 
-function addToFavorite() {
-    let wrapperElement = this.parentElement;
-    let favorites = JSON.parse(storage.getItem('favorites'));
-    favorites.push(wrapperElement.getAttribute('name'));
-    storage.setItem('favorites', JSON.stringify(favorites));
-    setBackground(wrapperElement, 'antiquewhite');
+    function addToFavorite() {
+        let wrapperElement = this.parentElement;
+        let favorites = storage.get('favorites');
+        favorites.push(wrapperElement.getAttribute('name'));
+        storage.set('favorites', favorites);
+        setBackground(wrapperElement, 'antiquewhite');
 
-    let listFavorites = document.getElementById('listFavorites');
-    let li = document.createElement('li');
-    li.onclick = removeFromFavorite;
-    let text = document.createTextNode(wrapperElement.getAttribute('name'));
-    li.appendChild(text);
-    listFavorites.appendChild(li);
-}
+        let listFavorites = document.getElementById('listFavorites');
+        let li = document.createElement('li');
+        li.onclick = removeFromFavorite;
+        let text = document.createTextNode(wrapperElement.getAttribute('name'));
+        li.appendChild(text);
+        listFavorites.appendChild(li);
+    }
 
-function removeFromFavorite() {
-    this.remove();
-    let favorites = JSON.parse(storage.getItem('favorites'));
-    favorites = favorites.filter(item => {
-        return item !== this.innerText;
-    });
-    storage.setItem('favorites', JSON.stringify(favorites));
-
-    document.querySelectorAll('#list li').forEach(element => {
-        if(element.getAttribute('name') === this.innerText) {
-            setBackground(element, 'none');
-        }
-    })
-}
-
-function onSubmit(event) {
-    event.preventDefault();
-    filter(document.querySelectorAll("#list li"), this.type.value, typeCollection);
-}
-
-function setTypeCollection(link) {
-    let types = [];
-    getPokemons(link).then(data => {
-        let pokemonData = JSON.parse(data);
-        pokemonData.types.forEach(item => {
-            types.push(item.type.name);
+    function removeFromFavorite() {
+        this.remove();
+        let favorites = storage.get('favorites');
+        favorites = favorites.filter(item => {
+            return item !== this.innerText;
         });
-        typeCollection.push({name: pokemonData.name, types: types});
-    });
-}
+        storage.set('favorites', favorites);
 
-function initFavorites() {
-    storage.setItem('favorites', JSON.stringify([]));
-}
+        document.querySelectorAll('#list li').forEach(element => {
+            if(element.getAttribute('name') === this.innerText) {
+                setBackground(element, 'none');
+            }
+        })
+    }
+
+    function onSubmit(event) {
+        event.preventDefault();
+        filter(document.querySelectorAll("#list li"), this.type.value, typeCollection);
+    }
+
+    function setTypeCollection(link) {
+        let types = [];
+        getPokemons(link).then(data => {
+            let pokemonData = JSON.parse(data);
+            pokemonData.types.forEach(item => {
+                types.push(item.type.name);
+            });
+            typeCollection.push({name: pokemonData.name, types: types});
+        });
+    }
+
+    function initFavorites() {
+        storage.set('favorites', []);
+    }
+})();
+
+
+
--- scripts/storage.js
+++ scripts/storage.js
@@ -1,5 +1,17 @@
 "use strict";
 
-let storage = localStorage;
+class Storage {
+    get(item) {
+        return JSON.parse(localStorage.getItem(item));
+    }
+
+    set(key, value) {
+        if(value instanceof Object) {
+            value = JSON.stringify(value);
+        }
+        localStorage.setItem(key, value);
+    }
+}
+let storage = new Storage();
 
 export default storage;
